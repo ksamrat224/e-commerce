aea7b683d653781681679b76f65665f2633a76e8
ksamrat224 <ksamrat224@gmail.com>
2025-04-01 17:16:20 +0545

    NavBar,Footer,Link and react icons

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,18 @@ import React from "react";
 import HomePage from "./pages/HomePage";
 import ErrorPage from "./pages/ErrorPage";
 import { BrowserRouter, Route, Routes } from "react-router";
+import NavBar from "./components/NavBar";
+import Footer from "./components/Footer";
 
 export const App = () => {
   return (
     <BrowserRouter>
+      <NavBar />
       <Routes>
         <Route path="/" element={<HomePage />} />
-        <Route path="*" element={<ErrorPage />} />
+        <Route path="/error" element={<ErrorPage />} />
       </Routes>
+      <Footer />
     </BrowserRouter>
   );
 };
