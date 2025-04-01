a1ca69927ba01d7fe318f40512fbe5cd5426378b
ksamrat224 <ksamrat224@gmail.com>
2025-04-01 17:41:23 +0545

    Login button updated

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { Link } from "react-router";
 import { CiShop } from "react-icons/ci";
+import { MdLogin } from "react-icons/md";
 
 const NavBar = () => {
   return (
@@ -19,19 +20,8 @@ const NavBar = () => {
             <Link className="mr-5 hover:text-gray-900">Third Link</Link>
             <Link className="mr-5 hover:text-gray-900">Fourth Link</Link>
           </nav>
-          <button className="inline-flex items-center bg-gray-100 border-0 py-1 px-3 focus:outline-none hover:bg-gray-200 rounded text-base mt-4 md:mt-0">
-            Button
-            <svg
-              fill="none"
-              stroke="currentColor"
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              strokeWidth={2}
-              className="w-4 h-4 ml-1"
-              viewBox="0 0 24 24"
-            >
-              <path d="M5 12h14M12 5l7 7-7 7" />
-            </svg>
+          <button className="inline-flex items-center bg-gray-100 border-0 py-1 px-3 focus:outline-none hover:bg-gray-400 rounded text-base mt-4 md:mt-0 font-bold text-gray-700 hover:text-white">
+            LogIn <MdLogin />
           </button>
         </div>
       </header>
