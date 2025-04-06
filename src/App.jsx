e890fadd838b0139f55939e4e5803735ecfa8ef7
ksamrat224 <ksamrat224@gmail.com>
2025-04-06 09:56:19 +0545

    data added into redux store

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,28 @@
-import React from "react";
+import React, { useEffect } from "react";
 import HomePage from "./pages/HomePage";
 import ErrorPage from "./pages/ErrorPage";
 import { BrowserRouter, Route, Routes } from "react-router";
 import NavBar from "./components/NavBar";
 import Footer from "./components/Footer";
+import axios from "axios";
+import { useDispatch } from "react-redux";
+import { addProducts } from "./store/productsSlice";
 
 export const App = () => {
+  const dispatch = useDispatch();
   const fetchProducts = async()=>{
     try{
-  
+      const response = await axios.get("https://fakestoreapi.com/products");
+      const data =  response.data;
+      dispatch(addProducts(data));
     }
     catch(error){
       console.log(error)
     }
   }
+  useEffect(()=>{
+   fetchProducts();
+  },[])
   return (
     <BrowserRouter>
       <NavBar/>
